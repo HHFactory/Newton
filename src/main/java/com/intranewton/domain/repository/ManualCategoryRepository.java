64cb082d6c39f1845159844a6af1210fa53cee78
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2015-12-11 21:48:20 +0900

    add マニュアルカテゴリ

--- src/main/java/com/intranewton/domain/repository/ManualCategoryRepository.java
+++ src/main/java/com/intranewton/domain/repository/ManualCategoryRepository.java
@@ -1,23 +1,17 @@
 package com.intranewton.domain.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.intranewton.domain.entity.Category;
+import com.intranewton.domain.entity.ManualCategory;
 import java.lang.Integer;
+import java.util.List;
 
 /**
- * m_categoryテーブルリポジトリ
+ * m_manual_categoryテーブルリポジトリ
  */
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-
-	//指定されたカテゴリ区分でカテゴリ一覧を取得する
-	@Query("SELECT c FROM Category c WHERE c.category_div =:category_div")
-	List<Category> findByCategory_div(@Param("category_div") Integer category_div);
-	
+public interface ManualCategoryRepository extends JpaRepository<ManualCategory, Integer>{
+	//カテゴリIDからカテゴリを取得する
+	List<ManualCategory> findById(Integer id);
 }
