62ac279afaa2d8e9159a153c3dad3d42efbfa1ef
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2015-12-11 19:00:28 +0900

    modify マニュアル、FAQごとにカテゴリを分離

--- src/main/java/com/intranewton/domain/entity/ManualCategory.java
+++ src/main/java/com/intranewton/domain/entity/ManualCategory.java
@@ -1,35 +1,51 @@
 package com.intranewton.domain.entity;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Where;
 
-import com.intranewton.domain.common.AbstractEntity;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
- * 現在未使用
+ * m_categoryテーブルエンティティ
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="m_manual_category")
+@Table(name="m_category")
 @Where(clause="status='valid'")
-public class ManualCategory extends AbstractEntity {
-	private String name;
-	@OneToMany(fetch=FetchType.LAZY)
-	@JoinColumn(name="m_manual_category_id")
-	private List<ManualList> manualLists;
+@Embeddable
+public class Category implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String status;
+	private Integer category_div;
+	private String category_name;
+	
+	@OneToMany
+	@JoinColumn(name="m_category_id")
+	private List<Manual> manuals;
+	@OneToMany
+	@JoinColumn(name="m_category_id")
+	private List<FAQ> faqs;
+	
+	@OneToMany(mappedBy="children")
+	List<CategoryRelations> children;
+	@OneToMany(mappedBy="parent")
+	List<CategoryRelations> parents;
 }
