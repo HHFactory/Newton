e530ef24d944440427d926c6ec81648aa041f99b
KMELab <k-matsunami@virtualex.co.jp>
2015-12-22 01:50:47 +0900

    bugfix 起動時のJPAエラー修正。クラス名を変更した場合はJPQLも変更する必要がある模様

--- src/main/java/com/intranewton/domain/repository/FaqRepository.java
+++ src/main/java/com/intranewton/domain/repository/FaqRepository.java
@@ -17,12 +17,12 @@ import com.intranewton.domain.entity.Faq;
 @Repository
 public interface FaqRepository extends JpaRepository<Faq, Integer>{
 	//役に立ったボタン押下時にuseful_countカラムをインクリメントする。
-	@Query("UPDATE FAQ faq SET faq.useful_count = faq.useful_count + 1 WHERE faq.id=?")
+	@Query("UPDATE Faq faq SET faq.useful_count = faq.useful_count + 1 WHERE faq.id=?")
 	@Modifying
 	@Transactional
 	Integer countUpUsefulCount(Integer id);
-	
+
 	//文字列検索用クエリ(elasticsearchに移行)
-	@Query("SELECT f FROM FAQ f where f.title like %:title% order by f.update_datetime asc")
+	@Query("SELECT f FROM Faq f where f.title like %:title% order by f.update_datetime asc")
 	List<Faq> searchFaq(@Param("title") String title);
 }
