37e287a3d3488198cef7c841f6d88de57fa06b70
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2016-01-24 02:25:21 +0900

    add whiteboard追加

---
+++ src/main/java/com/intranewton/domain/entity/WhiteBoard.java
@@ -0,0 +1,34 @@
+package com.intranewton.domain.entity;
+
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Where;
+
+import com.intranewton.domain.common.AbstractEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+/**
+ * m_white_boardエンティティクラス
+ *
+ */
+@Entity
+@Table(name="m_white_board")
+@Where(clause="status='valid'")
+@Data
+@EqualsAndHashCode(callSuper=false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class WhiteBoard extends AbstractEntity implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String content;
+	
+}
