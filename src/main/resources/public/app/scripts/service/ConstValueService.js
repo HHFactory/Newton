7c736aa80beae9f5e2f89f0450ef9c08481c19da
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2016-01-30 23:56:40 +0900

    modify hostname

--- src/main/resources/public/app/scripts/service/ConstValueService.js
+++ src/main/resources/public/app/scripts/service/ConstValueService.js
@@ -6,24 +6,25 @@
 */
 (function(){
 'use strict';	
+	var hostName = "https://hhfactory-newton.herokuapp.com";
 
 	angular.module('indexModule').value(
 		'constURI',{
-			'getFaqList':"http://localhost:8080/api/v1/faqs",
-			'postFaq':"http://localhost:8080/api/v1/faq",
-			'putFaqUseful':"http://localhost:8080/api/v1/faqs/",
-			'getModifyFaqList':"http://localhost:8080/api/v1/modifyfaqs",
-			'getNotificationList':"http://localhost:8080/api/v1/notifications",
-			'putNotification':"http://localhost:8080/api/v1/notification",
-			'getManualList':"http://localhost:8080/api/v1/manuals",
-			'postManual':"http://localhost:8080/api/v1/manual",
-			'putManual':"http://localhost:8080/api/v1/manual",
-			'getSkillList':"http://localhost:8080/api/v1/roles",
-			'getUserList':"http://localhost:8080/api/v1/users",//ToDO:スキルに応じたユーザ取得に切り替え
-			'getToDoList':"http://localhost:8080/api/v1/todos",
-			'getToDoListByTargetUser':"http://localhost:8080/api/v1/targettodos",
-			'postToDo':"http://localhost:8080/api/v1/todo",
-			'searchAPI':"http://localhost:8080/api/v1/elastic/querysearch"
+			'getFaqList': hostName + "/api/v1/faqs",
+			'postFaq': hostName + "/api/v1/faq",
+			'putFaqUseful': hostName + "/api/v1/faqs/",
+			'getModifyFaqList': hostName + "/api/v1/modifyfaqs",
+			'getNotificationList': hostName + "/api/v1/notifications",
+			'putNotification': hostName + "/api/v1/notification",
+			'getManualList': hostName +"/api/v1/manuals",
+			'postManual': hostName +"/api/v1/manual",
+			'putManual': hostName +"/api/v1/manual",
+			'getSkillList': hostName +"/api/v1/roles",
+			'getUserList': hostName +"/api/v1/users",
+			'getToDoList': hostName +"/api/v1/todos",
+			'getToDoListByTargetUser': hostName +"/api/v1/targettodos",
+			'postToDo': hostName +"/api/v1/todo",
+			'searchAPI': hostName +"/api/v1/elastic/querysearch"
 		}
 	);
 
