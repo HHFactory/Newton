6d8eed633d6be1f29116e2207d694ec7809ba3d6
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2016-02-02 12:43:37 +0900

    modify

--- src/main/java/com/intranewton/Application.java
+++ src/main/java/com/intranewton/Application.java
@@ -3,7 +3,6 @@ package com.intranewton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,12 +12,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
 @EnableJpaRepositories(includeFilters=@ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE, value=JpaRepository.class))
 @EnableElasticsearchRepositories(includeFilters=@ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE,value=ElasticsearchRepository.class))
 public class Application {
+	
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
 	}
