e39f283b10718f83d1314eb9ae7de93d590e508e
HHFactory <hidenori.horinouchi.vxc@gmail.com>
2016-02-06 08:01:23 +0900

    modify テーブル自動作成に設定を変更

--- src/main/java/com/intranewton/AppConfig.java
+++ src/main/java/com/intranewton/AppConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-@Configuration
+//@Configuration
 public class AppConfig {
 	@Autowired
 	DataSourceProperties properties;
--- src/main/java/com/intranewton/Application.java
+++ src/main/java/com/intranewton/Application.java
@@ -3,13 +3,14 @@ package com.intranewton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@Configuration
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @EnableJpaRepositories(includeFilters=@ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE, value=JpaRepository.class))
